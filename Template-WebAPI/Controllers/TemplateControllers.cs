cd9c98905f0be548ac195fc157a1d1c6efee60c9
FJvanEeden <fredz.eeden@gmail.com>
2020-03-24 16:57:21 +0200

    Created the entities repository and interface.

---
+++ Template-WebAPI/Controllers/TemplateControllers.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Template_WebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Template_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TemplateControllers : ControllerBase
+    {
+                      
+    }
+}

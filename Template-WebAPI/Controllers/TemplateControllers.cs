779b06202926f8d5128f6706f85a7cd35cc3b6b0
FJvanEeden <fredz.eeden@gmail.com>
2020-03-25 08:23:18 +0200

    Added Controller without CRUD methods (to be added with base repository)

--- Template-WebAPI/Controllers/TemplateControllers.cs
+++ Template-WebAPI/Controllers/TemplateControllers.cs
@@ -2,13 +2,27 @@
 using Template_WebAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Template_WebAPI.Repository;
 
 namespace Template_WebAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/templates/[controller]")]
     [ApiController]
     public class TemplateControllers : ControllerBase
     {
-                      
+        private readonly ITemplateRepository _templateRepository;
+
+        public TemplateControllers(ITemplateRepository templateRepository)
+        {
+            _templateRepository = templateRepository;
+        }
+
+        [HttpGet]
+
+        [HttpPut]
+
+        [HttpPost]
+
+        [HttpDelete]
     }
 }

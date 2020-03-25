235caf2bc1e5f08aeb494ae09cc670a9beea6060
FJvanEeden <fredz.eeden@gmail.com>
2020-03-25 10:42:58 +0200

    Created DB context for respository pattern.

---
+++ Template-WebAPI/DbContexts/MongoContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using Template_WebAPI.Interfaces;
+
+namespace Template_WebAPI.DbContexts
+{
+    public class MongoContext : IMongoContext
+    {
+        private IMongoDatabase _db { get; set; }
+        private MongoClient _mongoClient { get; set; }
+
+        private readonly IConfiguration _configuration;
+
+        public MongoContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            // Configuration to be injected later
+            _mongoClient = new MongoClient(_configuration[""]);
+            _db = _mongoClient.GetDatabase(_configuration[""]);
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string name)
+        {
+            return _db.GetCollection<T>(name);
+        }
+    }
+}
---
+++ Template-WebAPI/Interfaces/IMongoContext.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Template_WebAPI.Interfaces
+{
+    interface IMongoContext
+    {
+        IMongoCollection<T> GetCollection<T>(string name);
+    }
+}

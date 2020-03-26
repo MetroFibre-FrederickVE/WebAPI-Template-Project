b58703f44b6e2ffe6e1623e6552ed138b7db522b
FJvanEeden <fredz.eeden@gmail.com>
2020-03-26 07:36:47 +0200

    Removed unnececary code.

--- Template-WebAPI/Models/TemplateDbSettings.cs
+++ Template-WebAPI/Models/TemplateDbSettings.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Template_WebAPI.Models
+﻿namespace Template_WebAPI.Models
 {
     public class TemplateDbSettings : ITemplateDbSettings
     {

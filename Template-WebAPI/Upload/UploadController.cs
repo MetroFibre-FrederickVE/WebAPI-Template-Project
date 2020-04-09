c270f2e23399356ae4993de36905278e13617a84
FJvanEeden <fredz.eeden@gmail.com>
2020-04-09 18:10:50 +0200

    Reverted back to "Commit 025e64a7"

--- Template-WebAPI/Upload/UploadController.cs
+++ Template-WebAPI/Upload/UploadController.cs
@@ -1,11 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.IO;
 using System.Net.Http;
-using System.Threading.Tasks;
-using System.Web;
+using System.Xml.Linq;
+using Template_WebAPI.Interfaces;
+using Template_WebAPI.Upload;
 
-namespace Template_WebAPI.Upload
+namespace Template_WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
@@ -17,64 +16,16 @@ namespace Template_WebAPI.Upload
             _uploadRepository = uploadRepository;
         }
 
-        [HttpPost]
-        public Task UploadFile()
+        [Route("sendXML")]
+        public async void SendXML()
         {
-            // Old framework call? - "Request.Files" Moved to "Request.Form.Files"
-            foreach (var file in Request.Form.Files)
-            {
-                
-            }
+            var request = new HttpRequestMessage();
 
-            var FileDataContent = Request.Form.Files;
-            if (FileDataContent != null && FileDataContent.Length > 0)
-            {
-                // Old framework call? - ".InputStream" Moved to ".OpenReadStream"
-                var stream = FileDataContent.OpenReadStream;
-                var fileName = Path.GetFileName(FileDataContent.FileName);
-
-                //var UploadPath = MapPath("~/dir/api/upload/");
-                //Directory.CreateDirectory(UploadPath);
-                string path = Path.Combine("~/dir/api/upload/", fileName);
-                try
-                {
-                    if (System.IO.File.Exists(path))
-                        System.IO.File.Delete(path);
-                    using (var fileStream = System.IO.File.Create(path))
-                    {
-                        stream.CopyTo(fileStream);
-                    }
-                    Utils UT = new Utils();
-                    UT.MergeFile(path);
-                }
-                catch (IOException ex)
-                {
-
-                }
-            }
+            XDocument doc = XDocument.Load(await request.Content.ReadAsStreamAsync());
+            string fileName = "something.xml"; // insert filename spawner
+            string saveLoc = string.Format(@"", fileName); // insert path
+            doc.Save(saveLoc);
 
         }
-
-        [HttpPost]
-        public string MultiUpload()
-        {
-            var chunks = Request.InputStream;
-
-            string path = HttpContext.Current.Server.MapPath("~/App_Data/Uploads/Tamp");
-            string newpath = Path.Combine(path, Path.GetRandomFileName());
-
-            using (System.IO.FileStream fs = System.IO.File.Create(newpath))
-            {
-                byte[] bytes = new byte[77570];
-
-                int bytesRead;
-                while ((bytesRead = Request.InputStream.Read(bytes, 0, bytes.Length)) > 0)
-                {
-                    fs.Write(bytes, 0, bytesRead);
-                }
-            }
-            return "test";
-        }
-
     }
 }

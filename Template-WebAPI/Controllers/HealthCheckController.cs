e0d14ee883b2cd1a410e2877b2dacb1a5ba4e346
FJvanEeden <fredz.eeden@gmail.com>
2020-04-20 10:02:27 +0200

    Added health check for api. Added dockerfile and compose file.

---
+++ Template-WebAPI/Controllers/HealthCheckController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Template_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+    [HttpGet]
+    public ActionResult<string> GetSensorAsync()
+    {
+      return Ok("Template services up and running.");
+    }
+  }
+
+    
+}

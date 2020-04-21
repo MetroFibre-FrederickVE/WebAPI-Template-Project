3fff96f765602c1c1fac48973e2d1c95e62ba62d
FJvanEeden <fredz.eeden@gmail.com>
2020-04-21 09:22:10 +0200

    Added S3 upload support.

--- Template-WebAPI/Manager/AWSCloudFileManager.cs
+++ Template-WebAPI/Manager/AWSCloudFileManager.cs
@@ -1,24 +1,46 @@
+using System;
 using System.IO;
 using System.Threading.Tasks;
 using Amazon.S3;
+using Amazon.S3.Transfer;
 using Template_WebAPI.Model;
 
 namespace Template_WebAPI.Manager
 {
   public class AWSCloudFileManager : ICloudFileManager
   {
+    private const string bucketName = "firsttempbucket";
     private readonly IAmazonS3 awsS3;
-    public AWSCloudFileManager(IAmazonS3 s3)
+
+    public AWSCloudFileManager(IAmazonS3 s3Client)
     {
-      this.awsS3 = s3;
+      this.awsS3 = s3Client;
+      awsS3 = new AmazonS3Client();
     }
 
-    public Task UploadTemplateXMLFileAsync(Template template)
+    public async Task UploadTemplateXMLFileAsync(Template template)
     {
+
       var folderName = Path.Combine("Resources", "File");
       var pathToTemplateFile = Path.Combine(Directory.GetCurrentDirectory(), folderName);
       var fileName = $@"{pathToTemplateFile}\{template.Id}.xml";
-      throw new System.NotImplementedException();
+
+      try
+      {
+        var fileTransferUtility = new TransferUtility(awsS3);
+
+        await fileTransferUtility.UploadAsync(fileName, bucketName);
+        Console.WriteLine("File upload has been completed");
+      }
+      catch (AmazonS3Exception e)
+      {
+        Console.WriteLine("Error encountered on server. Message:'{0}' when writing an object", e.Message);
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine("Unknown encountered on server. Message:'{0}' when writing an object", e.Message);
+      }
+
     }
   }
 }

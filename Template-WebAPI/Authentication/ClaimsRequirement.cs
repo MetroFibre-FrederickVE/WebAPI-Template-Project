d4559c2c624872bc1fe49022d07e4ce0ef6fc7ec
FJvanEeden <fredz.eeden@gmail.com>
2020-05-22 09:56:32 +0200

    Added claims handler base.

--- Template-WebAPI/Authentication/ClaimsRequirement.cs
+++ Template-WebAPI/Authentication/ClaimsRequirement.cs
@@ -4,11 +4,11 @@ namespace Template_WebAPI.Authentication
 {
   public class ClaimsRequirement : IAuthorizationRequirement
   {
-    public string Role { get; }
-
     public ClaimsRequirement(string claimRole)
     {
       Role = claimRole;
     }
+
+    public string Role { get; private set; }
   }
 }
---
+++ Template-WebAPI/Authentication/ClaimsRequirementHandler.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Template_WebAPI.Authentication
+{
+  internal class ClaimsRequirementHandler : AuthorizationHandler<ClaimsRequirement>
+  {
+    protected override Task HandleRequirementAsync
+  }
+}

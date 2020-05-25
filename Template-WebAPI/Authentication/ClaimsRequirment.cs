39df8486232d104c9830ea9626eb8c21167a745c
FJvanEeden <fredz.eeden@gmail.com>
2020-05-25 18:13:55 +0200

    Added Json Deserializer and renamed class fields.

--- Template-WebAPI/Authentication/ClaimsRequirment.cs
+++ Template-WebAPI/Authentication/ClaimsRequirment.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Template_WebAPI.Authentication
 {
   public class ClaimsRequirment : IAuthorizationRequirement
   {
+    private readonly JsonSerializerSettings serializerSettings;
+
     public ClaimsRequirment(string role)
     {
       MatchingRole = role;
+
+      serializerSettings = new JsonSerializerSettings
+      {
+        ContractResolver = new DefaultContractResolver
+        {
+          NamingStrategy = new CamelCaseNamingStrategy()
+        },
+        Formatting = Formatting.Indented
+      };
     }
 
     public string MatchingRole { get; private set; }

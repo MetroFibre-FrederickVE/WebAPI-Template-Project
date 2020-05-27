f0b6b1d8093d0a787ea06bdf20c0e434230158c1
FJvanEeden <fredz.eeden@gmail.com>
2020-05-27 09:37:29 +0200

    Implemented a Try-Catch block for the handler. Extended the foreach loop to search ALL groups for all contained roles.

--- Template-WebAPI/Authentication/ClaimsRequirementHandler.cs
+++ Template-WebAPI/Authentication/ClaimsRequirementHandler.cs
@@ -10,32 +10,54 @@ namespace Template_WebAPI.Authentication
   {
     protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ClaimsRequirment policyRequirement)
     {
-      var tokenClaimsToList = context.User.Claims.Select(c => c.Value).ToList();
-
-      var claimsValue = tokenClaimsToList[4].Substring(0, tokenClaimsToList[4].Length);
-
-      var options = new JsonSerializerOptions
+      try
       {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = true
-      };
+        var tokenClaimsToList = context.User.Claims.Select(c => c.Value).ToList();
 
-      var claimsJsonModel = JsonSerializer.Deserialize<ClaimsFromToken>(claimsValue, options);
+        var claimsValue = tokenClaimsToList[4].Substring(0, tokenClaimsToList[4].Length);
 
-      List<string> collectionOfRolesFromToken = new List<string>();
+        var options = new JsonSerializerOptions
+        {
+          PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+          WriteIndented = true
+        };
 
-      foreach(var groupRole in claimsJsonModel.Groups.ElementAt(0).Roles)
+        var claimsJsonModel = JsonSerializer.Deserialize<ClaimsFromToken>(claimsValue, options);
+
+
+        // We could refactor the contains method to happen on the security groups roles collection.
+        // This would avoid creating a transient listOfTokenClaims collection just so that it can be searched.
+        List<string> collectionOfRolesFromToken = new List<string>(); //
+
+        // Is this hard coded to the first group only?
+        //foreach (var groupRole in claimsJsonModel.Groups.ElementAt(0).Roles) 
+        //{
+        //  var roleToString = groupRole.RoleName.ToString();
+        //  collectionOfRolesFromToken.Add(roleToString);
+        //}
+
+
+        foreach (var group in claimsJsonModel.Groups)
+        {
+          foreach (var role in group.Roles)
+          {
+            var roleToString = role.RoleName.ToString();
+            collectionOfRolesFromToken.Add(roleToString);
+          }
+        }
+
+        string policyRequirment = policyRequirement.MatchingRole;
+
+        if (collectionOfRolesFromToken.Contains(policyRequirment))
+        {
+          context.Succeed(policyRequirement);
+        }
+      }
+      catch
       {
-        var roleToString = groupRole.RoleName.ToString();
-        collectionOfRolesFromToken.Add(roleToString);
+        return Task.CompletedTask;
       }
 
-      string policyRequirment = policyRequirement.MatchingRole;
-
-      if (collectionOfRolesFromToken.Contains(policyRequirment))
-      {
-        context.Succeed(policyRequirement);
-      }
       return Task.CompletedTask;
     }
   }

371c24b92e10747ee679c06162b8e13c0d2a8594
FJvanEeden <fredz.eeden@gmail.com>
2020-05-27 10:17:25 +0200

    Refactored the role validation to check each incoming role against the policy requirment.

--- Template-WebAPI/Authentication/ClaimsRequirementHandler.cs
+++ Template-WebAPI/Authentication/ClaimsRequirementHandler.cs
@@ -12,6 +12,8 @@ namespace Template_WebAPI.Authentication
     {
       try
       {
+        string policyRequirment = policyRequirement.MatchingRole;
+
         var tokenClaimsToList = context.User.Claims.Select(c => c.Value).ToList();
 
         var claimsValue = tokenClaimsToList[4].Substring(0, tokenClaimsToList[4].Length);
@@ -24,40 +26,21 @@ namespace Template_WebAPI.Authentication
 
         var claimsJsonModel = JsonSerializer.Deserialize<ClaimsFromToken>(claimsValue, options);
 
-
-        // We could refactor the contains method to happen on the security groups roles collection.
-        // This would avoid creating a transient listOfTokenClaims collection just so that it can be searched.
-        List<string> collectionOfRolesFromToken = new List<string>(); //
-
-        // Is this hard coded to the first group only?
-        //foreach (var groupRole in claimsJsonModel.Groups.ElementAt(0).Roles) 
-        //{
-        //  var roleToString = groupRole.RoleName.ToString();
-        //  collectionOfRolesFromToken.Add(roleToString);
-        //}
-
-
         foreach (var group in claimsJsonModel.Groups)
         {
           foreach (var role in group.Roles)
           {
-            var roleToString = role.RoleName.ToString();
-            collectionOfRolesFromToken.Add(roleToString);
+            if (role.RoleName.ToString() == policyRequirment)
+            {
+              context.Succeed(policyRequirement);
+            }
           }
-        }
-
-        string policyRequirment = policyRequirement.MatchingRole;
-
-        if (collectionOfRolesFromToken.Contains(policyRequirment))
-        {
-          context.Succeed(policyRequirement);
         }
       }
       catch
       {
         return Task.CompletedTask;
       }
-
       return Task.CompletedTask;
     }
   }

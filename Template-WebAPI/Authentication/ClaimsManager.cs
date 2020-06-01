2be6b19f4b488c8329a7b6c1a11df9fd3b890e8c
FJvanEeden <fredz.eeden@gmail.com>
2020-06-01 18:01:36 +0200

    Expanded claims manager.

--- Template-WebAPI/Authentication/ClaimsManager.cs
+++ Template-WebAPI/Authentication/ClaimsManager.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MPU.MicroServices.StandardLibrary.CloudMessaging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Template_WebAPI.Authentication
@@ -8,10 +10,22 @@ namespace Template_WebAPI.Authentication
   public class ClaimsManager
   {
     private readonly ICloudMessageBus cloudMessageBus;
+    private readonly IClaimsRepository claimsRepository;
 
+    public ClaimsManager(ICloudMessageBus cloudMessageBus, IClaimsRepository claimsRepository)
+    {
+      this.cloudMessageBus = cloudMessageBus;
+      this.claimsRepository = claimsRepository;
+    }
+
+    // FUNC
     public async Task securityClaimsReturnValue(SecurityClaims security)
     {
-      await Task.Delay(1000);
+      // Expand to execute update
+
+      
+
+      await claimsRepository.UpdateAsync(security.Id, security);
     }
 
     public async Task UpdateDBFromSQSMessageBody()
@@ -19,20 +33,7 @@ namespace Template_WebAPI.Authentication
       CancellationTokenSource cts = new CancellationTokenSource();
       CancellationToken token = cts.Token;
 
-      //
-      //Func<SecurityClaims, Task> func = sc => Task.FromResult(sc) ;
-
       await cloudMessageBus.StartQueueSubscription<SecurityClaims>("UpdateSecurityClaims", securityClaimsReturnValue, token);
-
-      // MessageBody-TO-Document in Collection
-
-      // func?
-      //var idFromMessage = await func.messageBody.Id;
-      //var bodyFromMessage = await func.messageBody;
-
-      // Loop through all documents?
-      //var objectId = new ObjectId(idFromMessage);
-      //await _dbCollection.UpdateOneAsync(Builders<SecurityClaims>.Filter.Eq("_id", objectId), bodyFromMessage);
 
     }
   }
